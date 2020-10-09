3aa404b16c73166fd0fe41979c023e425af5845e
EC2 Default User <ec2-user@ip-172-31-70-202.ec2.internal>
2020-10-09 20:35:27 +0000

    new script

--- polus-threshold-apply1-plugin/src/main.py
+++ polus-threshold-apply1-plugin/src/main.py
@@ -9,7 +9,6 @@ import javabridge as jutil
 import argparse, logging, subprocess, time, multiprocessing, sys
 from pathlib import Path
 
-
 if __name__=="__main__":
     # Initialize the logger
     logging.basicConfig(format='%(asctime)s - %(name)-8s - %(levelname)-8s - %(message)s',
@@ -57,13 +56,17 @@ if __name__=="__main__":
     ##import things after imagej.init otherwise imagej doesn't work!!
     import imglyb
     from imglyb import util
-    from jnius import PythonJavaClass, java_method, autoclass, cast 
+    from jnius import JavaClass, MetaJavaClass, PythonJavaClass, java_method, autoclass, cast
+    #func_from = getattr(autoclass('some.java.Class'), 'from')
+    #func_from()
 
     # Loop through files in images image collection and process
     for f in images_files:
         # Load an image
         br = BioReader(Path(inpDir).joinpath(f))
-        image = np.squeeze(br.read_image())
+        a = br.read_image(X=(0,256),Y=(0,256))
+        #print(a)        
+        image = np.squeeze(a)
 
          # Create a RandomAccessibleInterval view for the image inside of ImageJ
         image_rai = ij.py.to_java(image)
@@ -71,49 +74,55 @@ if __name__=="__main__":
 
         # Create the output numpy array
         output = np.zeros(image.shape,dtype=image.dtype)
-            
-      
-        ##Type conversions
 
         arg_out = ij.op().transform().flatIterableView(ij.py.to_java(output))
         arg_in = ij.op().transform().flatIterableView(image_rai)
-        arg_number = float(threshVal)
-        arg_realtype = ij.py.to_java(arg_number)
-
-        #print(type(arg_realtype))
-
-        #changed = cast('net/imglib2/type/numeric/RealType', arg_realtype)
-
-        FloatType = autoclass('net.imglib2.type.numeric.integer.UnsignedShortType')
-        thresh_val = FloatType()
+        UnsignedShorterType = autoclass('net.imglib2.type.numeric.integer.UnsignedShortType')
+        thresh_val = UnsignedShorterType()
         thresh_val.set(float(threshVal))
         output = ij.op().threshold().apply(arg_in, thresh_val)
 
+
+        print('type(output): {}'.format(type(output)))
+        #print('output.getClass(): {}'.format(output.getClass()))
+        print('dir(output): {}'.format(dir(output)))
+        cursor = output.cursor()
+        print(dir(cursor))
+
+        print(output.size())
+
+
+
+        output_np = np.zeros(((256,256,1,1,1)),dtype=image.dtype)
+        # print(br.num_x())
+        for x in range(256):
+            # print(x)
+            for y in range(256):
+                a = np.uint8(cursor.next().toString()) 
+
+        print(a)
+'''
+                
+         
+        print(output_np)     
+
+        #output = ij.py.new_numpy_image(output)
         
-
-        converted = ij.op().convert().uint8(output)
-
-        print(type(converted))
-
-        out = ij.py.new_numpy_image(converted)
-
-
-        
-        output = np.reshape(out,(br.num_y(),br.num_x(),br.num_z(),1,1))
+        #output = np.reshape(output,(br.num_y(),br.num_x(),br.num_z(),1,1))
         
         
             
         # Write the output
-        bw = BioWriter(str(Path(outDir).joinpath(f)),image=output, metadata=br.read_metadata())
-        bw.write_image(output)
+        bw = BioWriter(str(Path(outDir).joinpath(f)),image=output_np, metadata=br.read_metadata())
+        bw.write_image(output_np)
         bw.close_image()
         
         
            
 # Read an OME tiled tiff
-br = BioReader('polus-plugins/inputs/r001_z000_y009_x012_c000.ome.tif')
-image = np.squeeze(br.read_image()) # squeeze to 2-dimensions
-Two_out = np.squeeze(output)
+br = BioReader('INP1/r001_z000_y009_x012_c000.ome.tif')
+image = np.squeeze(br.read_image(X=(0,256),Y=(0,256))) # squeeze to 2-dimensions
+Two_out = np.squeeze(output_np)
 
 
 # Display the original and processed images
@@ -121,4 +130,7 @@ fig,ax = plt.subplots(1,2)
 ax[0].imshow(image)
 ax[1].imshow(Two_out)
 plt.show()
-plt.savefig('new.png')
+plt.savefig('converted1.png')
+
+'''
+
